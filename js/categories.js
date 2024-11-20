bfaae4c6a3d121c4722994741b16383684890b57
arielaaaa <alehrer@udesa.edu.ar>
2024-11-20 17:58:24 -0300

    pagina categoria html y js hecho(sin link). css basico hecho

---
+++ js/categories.js
@@ -0,0 +1,18 @@
+fetch('https://dummyjson.com/recipes/tags')
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    const categorySection = document.querySelector('.categoryPage')
+    let categories = ''
+    for (let i = 0; i < data.length; ++i){
+        categories += `<article>
+                            <p><a href=""> ${data[i]} </a></p>
+                        </article>` //FILL IN THE LINK IS ARTICLE THE RIGHT THING HERE??
+    }
+    categorySection.innerHTML = categories
+    
+})
+.catch(function(error){
+    console.log(`The error is ${error}`);
+})
