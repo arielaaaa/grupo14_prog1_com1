33afd844f64a12933800ae01ed070b86c75b2892
arielaaaa <alehrer@udesa.edu.ar>
2024-11-21 10:38:35 -0300

    login html y js hecho, css basico aplicado

---
+++ js/login.js
@@ -0,0 +1,27 @@
+// Checks the searchbar has more than 3 characters
+//IS THERE AWAY AROUND HAVING THE SAME THING ON EVERY JS FOR THE SEARCH BAR??????   
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    if (input.value == '' || input.value.length < 3) {
+        alert('Search must contain atleast 3 characters')
+    }else{
+        this.submit()
+    }
+})
+//Checks email for contents
+//NOT WORKING FIX FIX FIX
+const login = document.querySelector('.logReg form')
+const email = document.querySelector('#email')
+const psw = document.querySelector('#psw')
+login.addEventListener('submit', function (event) {
+    event.preventDefault();
+    if (email.value == '') {
+        alert('Email must be filled in')
+    }else if(psw.value == ''){//There will only be one alert at a time, if there is neither an email or password it will only alert about the missing email.
+        alert('Password must be filled in')
+    }else{
+        this.submit()
+    }
+})
